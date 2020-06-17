c5182ede2a1bb577f0181164c325bbbe04153848
balovbohdan <balovbohdan@gmail.com>
2020-06-17 08:57:25 +0300

    path aliases updated

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-import * as T from './types';
-
-const environment: T.Environment = {
-  DEBUG_MODE: true,
-  PRODUCTION: true,
-  MAPBOXGL_ACCESS_TOKEN: 'pk.eyJ1IjoiYmFsb3Zib2hkYW4iLCJhIjoiY2thdndoZDh6MWhpdTJ0bXNxNnJodGhoNSJ9.VUsm4M-674fgBmSu-GGlGw',
-};
-
-export default environment;
--- src/environments/types.ts
+++
@@ -1,5 +0,0 @@
-export type Environment = {
-  DEBUG_MODE: boolean;
-  PRODUCTION: boolean;
-  MAPBOXGL_ACCESS_TOKEN: string;
-};
