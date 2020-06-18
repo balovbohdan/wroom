108595ab9a672fbf6a9c8548eb5bbfbc068b6631
balovbohdan <balovbohdan@gmail.com>
2020-06-18 17:52:26 +0300

    id:Sw46anUx feat(eject-create-react-app): create-react-app ejected

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parserPreset: {
     parserOpts: {
-      headerPattern: /^id:(\d+) (\w+)\((.+)\): (.+)$/,
+      headerPattern: /^id:([\da-zA-Z])+ (\w+)\((.+)\): (.+)$/,
       headerCorrespondence: ['ticket', 'type', 'scope', 'subject'],
     },
   },
